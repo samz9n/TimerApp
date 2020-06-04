4dd46d856dd694b7e06f6eacc0dce1e56c4b4981
samz9n <samuel.langhoff@outlook.com>
2020-06-04 14:47:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+class Timer {
+	constructor(durationInput, startBtn, pauseBtn) {
+		this.durationInput = durationInput;
+		this.startBtn = startBtn;
+		this.pauseBtn = pauseBtn;
+
+		this.startBtn.addEventListener('click', this.start);
+		this.pauseBtn.addEventListener('click', this.pause);
+	}
+
+	start = () => {
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	tick = () => {
+		console.log('tick');
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+}
+
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
